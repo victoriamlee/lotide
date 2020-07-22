a4632d12d5ca43559d65a1acd6e9f36c2c864933
Victoria Lee <victorialee72@live.com>
2020-07-22 00:28:17 +0000

    Created eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,39 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let counter = 0;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        counter++;
+      }
+    }   
+    if (counter === array1.length) {
+      console.log(true);  
+    } else {
+      console.log(false);
+    }
+  } else {
+    console.log(false);
+  }
+};
+
+      
+      
+    
+
+// TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
