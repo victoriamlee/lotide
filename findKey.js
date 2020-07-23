274067b8a8e0c299378f4c2513d357fcb460c76d
Victoria Lee <victorialee72@live.com>
2020-07-23 20:41:14 +0000

    created findKey function

---
+++ findKey.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  const arrOne = Object.keys(object);
+  const arrTwo = Object.values(object);
+  for (let i = 0; i < arrTwo.length; i++) {
+    if (callback(arrTwo[i])) {
+      return arrOne[i];
+    }
+  }
+};
+  
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
