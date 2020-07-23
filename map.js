4d72949e340d1d64ce0eacf293860f28d1477821
Victoria Lee <victorialee72@live.com>
2020-07-23 19:34:49 +0000

    Created map function

---
+++ map.js
@@ -0,0 +1,47 @@
+const eqArrays = function(array1, array2) {
+  let counter = 0;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        counter++;
+      }
+    }   
+    if (counter === array1.length) {
+      return true;  
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if(eqArrays (actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word.length < 5);
+const results3 = map(words, word => word !== 'to');
+const results4 = map(words, word => word[1]);
+
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(results2, [ false, false, true, false, true ]);
+assertArraysEqual(results3,[ true, true, false, true, true ]);
+assertArraysEqual(results4,[ 'r', 'o', 'o', 'a', 'o' ]);
