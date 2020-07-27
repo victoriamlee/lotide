31438e2814703ed995c671297e258b54735145c7
Victoria Lee <victorialee72@live.com>
2020-07-27 17:23:35 +0000

    Refactored code

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let counter = 0;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        counter++;
-      }
-    }   
-    if (counter === array1.length) {
-      return true;  
-    } else {
-      return false;
-    }
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function (actual, expected) {
-  if(eqArrays (actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(arr) {
   let newArr = [];
   if (arr.length < 3) {
@@ -41,10 +15,4 @@ const middle = function(arr) {
   }
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-
+module.exports = middle;
