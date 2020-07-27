3fb4bf4ffaa92f352351e4a7db46ef90fe33e0be
Victoria Lee <victorialee72@live.com>
2020-07-27 20:02:14 +0000

    Exported function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -53,3 +53,5 @@ const assertObjectsEqual = function(actual, expected) {
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertObjectsEqual(cd, dc); // print true
+
+module.exports = assertObjectsEqual;
