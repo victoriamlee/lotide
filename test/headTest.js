62cc1977d4b8ed7304393cdf7189d76e7eaf6606
Victoria Lee <victorialee72@live.com>
2020-07-27 19:02:03 +0000

    Refactored code

--- test/headTest.js
+++ test/headTest.js
@@ -8,7 +8,7 @@ describe("#head", () => {
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5'); 
   });
-  it("returns \"Hello\" for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
   });
 });
