c1858f2aba204aef0e9de749cb4768c06f8e4d5b
Victoria Lee <victorialee72@live.com>
2020-07-27 18:38:22 +0000

    Edited index

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const letterPositions = require('./letterPositions');
 const map = require('./map');
-const reverse = require('./reverse');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
