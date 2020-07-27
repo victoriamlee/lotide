c745c10eb0784c549f8b2214411c2b2c3531f7a4
Victoria Lee <victorialee72@live.com>
2020-07-27 19:01:00 +0000

    Refactored the code

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,10 +2,10 @@ const assert = require('chai').assert;
 const tail   = require('../tail');
 
 describe("#tail", () => {
-  it("returns [\"Lighthouse\", \"Labs\"] for [\"Yo Yo\", \"Lighthouse\", \"Labs\"]", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
     assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
   });
-  it("returns [\"Orange\", \"Apple\", \"Mango\"] for [\"Banana\", \"Orange\", \"Apple\", \"Mango\"]", () => {
+  it("returns ['Orange', 'Apple', 'Mango'] for ['Banana', 'Orange', 'Apple', 'Mango']", () => {
     assert.deepEqual(tail(["Banana", "Orange", "Apple", "Mango"]), ["Orange", "Apple", "Mango"]); 
   });
 });
