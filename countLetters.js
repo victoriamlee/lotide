7d4bd5ca62c3b4c4a28604cd38216bab6d5932e2
Victoria Lee <victorialee72@live.com>
2020-07-28 14:30:03 +0000

    Refactored code

--- countLetters.js
+++ countLetters.js
@@ -1,12 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(string) {
   const objCounter = {};
   const letters = string.split(' ').join('');
