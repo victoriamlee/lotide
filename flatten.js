86667eeea873cddfecab5054f1df966827f075df
Victoria Lee <victorialee72@live.com>
2020-10-13 14:58:28 -0400

    Required eqArrays function

--- flatten.js
+++ flatten.js
@@ -1,26 +1,10 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function (actual, expected) {
   if(eqArrays (actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  let counter = 0;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        counter++;
-      }
-    }   
-    if (counter === array1.length) {
-      return true;  
-    } else {
-      return false;
-    }
-  } else {
-    return false;
   }
 };
 
